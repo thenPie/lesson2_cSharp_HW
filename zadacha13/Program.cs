d2bf6960306541b90edc6cd580e6783917134eac
thenPie <118397975+thenPie@users.noreply.github.com>
2022-12-04 00:55:14 +0300

    did zadacha13

---
+++ zadacha13/Program.cs
@@ -0,0 +1,16 @@
+﻿int InputNum() {
+    Console.Write("Input a number: ");
+    int num = int.Parse(Console.ReadLine());
+    return num;
+}
+
+void Show3rdD(int number) {
+    String str = number.ToString();
+    if (str.Length < 3) {
+        Console.WriteLine("The 3rd digit doesn't exist.");
+    } else {
+        Console.WriteLine("The 3rd digit is {0}.", str[2]);
+    }
+}
+
+Show3rdD(InputNum());
